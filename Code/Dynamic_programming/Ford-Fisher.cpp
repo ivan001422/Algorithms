5e71883e8e4302446bc6cd5b6325574f9e9b30da
Новиков Иван <148786186+ivan001422@users.noreply.github.com>
2026-04-12 22:59:40 +0300

    Update Ford-Fisher.cpp

--- Code/Dynamic_programming/Ford-Fisher.cpp
+++ Code/Dynamic_programming/Ford-Fisher.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
 
+using namespace std;
+
+bool is_different(char x, char y) {
+    return not(x == y);
+}
+
+int main() {
+    char x[] = "hello";
+    char y[] = "hel";
+
+    int matrix[6][4] = { 0 };  
+
+    for (int i = 0; i != 6; i++) matrix[i][0] = i;
+    for (int i = 0; i != 4; i++) matrix[0][i] = i;
+
+    // Алгоритм Форда-Фишера
+    for (int i = 1; i != 6; ++i) {
+        for (int j = 1; j != 4; ++j) {
+            matrix[i][j] = min(
+                min(matrix[i - 1][j], matrix[i][j - 1]) + 1,
+                matrix[i - 1][j - 1] + is_different(x[i - 1], y[j - 1])
+            );
+        }
+    }
+
+    // Вывод матрицы
+    for (int i = 0; i != 6; ++i) {
+        for (int j = 0; j != 4; ++j) {
+            cout << matrix[i][j] << " ";  
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
